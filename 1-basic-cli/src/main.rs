27e4c38a87a4a14e7dda2ba7c6e493069c0ca8fc
Sacha Pignot <Sacha.pignot@protonmail.com>
2024-10-08 15:21:59 -0400

    ✨ feat(project 1): Add basic cli project

--- 1-basic-cli/src/main.rs
+++ 1-basic-cli/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-  println!("Basic CLI application");
+  let mut args = std::env::args();
+
+  let path = args.nth(1);
+  match path {
+    Some(path) => println!("{}", read_file(&path)),
+    None => println!("Please supply a path!"),
+  }
+}
+
+fn read_file(path: &str) -> String {
+  let content_result = std::fs::read_to_string(path);
+
+  match content_result {
+    Ok(content) => content,
+    Err(error) => format!("Problem opening the file: {}", error.kind()),
+  }
 }
